b9a1d201d519501794d74b04e95cd4927971d7bc
jonas Firmino Soares <jonas.soares@dock.tech>
2022-09-07 11:29:13 -0300

    Dao Factory Implementation 2

--- src/application/Programapp.java
+++ src/application/Programapp.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Programapp {
 		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date (), 300.00, obj);
 		
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
