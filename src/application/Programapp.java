f849f330cec571496a59a0dec02cf73688ca5f59
jonas Firmino Soares <jonas.soares@dock.tech>
2022-09-07 13:02:51 -0300

    SellerDaoJDDBC findByID implemetantion

--- src/application/Programapp.java
+++ src/application/Programapp.java
@@ -1,24 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Programapp {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date (), 300.00, obj);
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 
 	}
 
