60f71fbd4686f645c9531c4d06fcdfc271e463ac
jonas Firmino Soares <jonas.soares@dock.tech>
2022-09-07 16:54:14 -0300

    Program app rebuild

--- src/application/Programapp.java
+++ src/application/Programapp.java
@@ -9,9 +9,8 @@ public class Programapp {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		System.out.println("=== TEST 1: Seller find by Id ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 		
