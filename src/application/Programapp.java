c778bda2bb17f638d2751431169f5cfe641f3c11
jonas Firmino Soares <jonas.soares@dock.tech>
2022-09-07 10:24:23 -0300

    Seller entities class

--- src/application/Programapp.java
+++ src/application/Programapp.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Programapp {
 
@@ -8,6 +11,10 @@ public class Programapp {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date (), 300.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
