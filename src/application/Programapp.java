3cf857d6479071a5ab73b9ec6c75cf3838eb95da
jonas Firmino Soares <jonas.soares@dock.tech>
2022-09-08 19:39:40 -0300

    Insert update

--- src/application/Programapp.java
+++ src/application/Programapp.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,12 @@ public class Programapp {
 		for (Seller obj : list2) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: Seller Insert ===");
+		Seller newSeller = new Seller(null, "Gregorio alves", "Greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted! New Id = " + newSeller.getId());
 		
 	}
 
